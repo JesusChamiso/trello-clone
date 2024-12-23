22ddeef60f177c2f344f34f53a9ea6e06127cb0b
JesusChamiso <jesus.chamiso@hotmail.com>
2024-12-23 19:57:41 -0400

    FontAwesome Icons

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,13 +1,17 @@
+import { faBell,faInfoCircle } from '@fortawesome/free-solid-svg-icons';
 import { Component } from '@angular/core';
 import { BtnComponent } from '../btn/btn.component';
-import {OverlayModule} from '@angular/cdk/overlay';
+import { OverlayModule } from '@angular/cdk/overlay';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome'
 
 @Component({
   selector: 'app-navbar',
-  imports: [BtnComponent, OverlayModule],
+  imports: [BtnComponent, OverlayModule, FontAwesomeModule],
   templateUrl: './navbar.component.html'
 })
 export class NavbarComponent {
+  faBell = faBell;
+  faInfoCircle = faInfoCircle;
   isOpenProfile = false;
   isOpenBody = false;
 
--- src/app/pages/boards/boards.component.ts
+++ src/app/pages/boards/boards.component.ts
@@ -1,11 +1,19 @@
+import { faB } from './../../../../node_modules/@fortawesome/free-solid-svg-icons/faB.d';
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faTrello } from '@fortawesome/free-brands-svg-icons';
+import { faBox, faWaveSquare, faClock } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-boards',
-  imports: [NavbarComponent],
+  imports: [NavbarComponent,FontAwesomeModule],
   templateUrl: './boards.component.html'
 })
 export class BoardsComponent {
+  faTrello = faTrello;
+  faBox = faBox;
+  faWaveSquare = faWaveSquare;
+  faClock = faClock;
 
 }
