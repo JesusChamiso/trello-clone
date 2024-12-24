547e8142dbc8f83628cff96b251496f12274e69f
JesusChamiso <jesus.chamiso@hotmail.com>
2024-12-23 22:20:16 -0400

    CDK Accordion

--- src/app/pages/boards/boards.component.ts
+++ src/app/pages/boards/boards.component.ts
@@ -1,13 +1,13 @@
-import { faB } from './../../../../node_modules/@fortawesome/free-solid-svg-icons/faB.d';
 import { Component } from '@angular/core';
 import { NavbarComponent } from '../../components/navbar/navbar.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faTrello } from '@fortawesome/free-brands-svg-icons';
-import { faBox, faWaveSquare, faClock } from '@fortawesome/free-solid-svg-icons';
+import { faBox, faWaveSquare, faClock, faAngleDown, faAngleUp, faGear, faUsers, faBorderAll, faHeart } from '@fortawesome/free-solid-svg-icons';
+import {CdkAccordionModule} from '@angular/cdk/accordion';
 
 @Component({
   selector: 'app-boards',
-  imports: [NavbarComponent,FontAwesomeModule],
+  imports: [NavbarComponent,FontAwesomeModule, CdkAccordionModule],
   templateUrl: './boards.component.html'
 })
 export class BoardsComponent {
@@ -15,5 +15,69 @@ export class BoardsComponent {
   faBox = faBox;
   faWaveSquare = faWaveSquare;
   faClock = faClock;
+  faAngleDown = faAngleDown;
+  faAngleUp = faAngleUp;
+  faHeart = faHeart;
+  faBorderAll = faBorderAll
+  faUsers = faUsers;
+  faGear = faGear;
+
+  items = [
+    { label: 'Item 1', 
+      items:[
+        {
+          label: 'Sub Item 1.1'
+        },{
+          label: 'Sub Item 1.2'
+        },{
+          label: 'Sub Item 1.3'
+        }
+      ]
+    },
+    { label: 'Item 2', 
+      items:[
+        {
+          label: 'Sub Item 2.1'
+        },{
+          label: 'Sub Item 2.2'
+        },{
+          label: 'Sub Item 2.3'
+        }
+      ]
+    },
+    { label: 'Item 3', 
+      items:[
+        {
+          label: 'Sub Item 3.1'
+        },{
+          label: 'Sub Item 3.2'
+        },{
+          label: 'Sub Item 3.3'
+        }
+      ]
+    },
+    { label: 'Item 4', 
+      items:[
+        {
+          label: 'Sub Item 4.1'
+        },{
+          label: 'Sub Item 4.2'
+        },{
+          label: 'Sub Item 4.3'
+        }
+      ]
+    },
+    { label: 'Item 5', 
+      items:[
+        {
+          label: 'Sub Item 5.1'
+        },{
+          label: 'Sub Item 5.2'
+        },{
+          label: 'Sub Item 5.3'
+        }
+      ]
+    },
+  ];
 
 }
