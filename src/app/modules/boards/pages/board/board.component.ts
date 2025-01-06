0469522019278a0f25459854419f4c0d13dda1f6
JesusChamiso <jesus.chamiso@hotmail.com>
2025-01-06 12:11:05 -0400

    detectar movimiento de cards

--- src/app/modules/boards/pages/board/board.component.ts
+++ src/app/modules/boards/pages/board/board.component.ts
@@ -63,6 +63,11 @@ export class BoardComponent implements OnInit {
         event.currentIndex
       );
     }
+    const rta = this.boardService.getPosition(
+      event.container.data,
+      event.currentIndex
+    );
+    console.log(rta);
   }
 
   addColumn() {
--- src/app/services/boards.service.ts
+++ src/app/services/boards.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { Board } from '../models/boards.model';
 import { environment } from '../../environments/environments';
 import { checkToken } from '../interceptors/token.interceptor';
+import { Card } from '../models/card.model';
 
 @Injectable({
   providedIn: 'root',
@@ -14,5 +15,27 @@ export class BoardsService {
     return this.http.get<Board>(`${this.apiUrl}boards/${id}`, {
       context: checkToken(),
     });
+  }
+
+  getPosition(cards: Card[], currentIndex: number) {
+    // Card Nuevo en una lista
+    if (cards.length === 1) {
+      return 'New Card';
+    }
+    // Card en la primera posición
+    if (cards.length > 1 && currentIndex === 0) {
+      return 'Top Card';
+    }
+    // Card en la mitad de la lista
+    const lastIndex = cards.length - 1;
+    if (cards.length > 2 && currentIndex > 0 && currentIndex < lastIndex) {
+      return 'Middle Card';
+    }
+    // Card en la última posición
+    if (cards.length > 1 && currentIndex === lastIndex) {
+      return 'Bottom Card';
+    }
+
+    return 0;
   }
 }
